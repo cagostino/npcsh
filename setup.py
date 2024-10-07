a72a5dd6571d06122bd110c4fd42e8611bf26b86
Chris Agostino <cjp.agostino@gmail.com>
2024-10-07 16:05:11 -0400

    changed verison

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ extra_files = package_files("npcsh/npc_profiles")
 
 setup(
     name="npcsh",
-    version="0.1.5",
+    version="0.1.6",
     packages=find_packages(exclude=["tests*"]),
     install_requires=[
         "jinja2",
