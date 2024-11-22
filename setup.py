2683b269c70907d0a3859ee4f8c3010c3b41de9d
Chris Agostino <cjp.agostino@gmail.com>
2024-11-22 14:04:10 -0500

    version number

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ extra_files = package_files("npcsh/npc_team/")
 
 setup(
     name="npcsh",
-    version="0.2.1",
+    version="0.2.2",
     packages=find_packages(exclude=["tests*"]),
     install_requires=[
         "anthropic",
