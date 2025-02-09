4b39668d59df6ae2bcfd6e2917b4bb3f25e62e81
Chris Agostino <cjp.agostino@gmail.com>
2025-02-09 01:31:12 -0500

    updating pyver

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ extra_files = package_files("npcsh/npc_team/")
 
 setup(
     name="npcsh",
-    version="0.3.2",
+    version="0.3.3",
     packages=find_packages(exclude=["tests*"]),
     install_requires=[
         "redis",
