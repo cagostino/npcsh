0396567db8331147a157f36f38bd5a67414854c6
Chris Agostino <cjp.agostino@gmail.com>
2025-03-18 02:02:58 -0400

    pyver

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ extra_files = package_files("npcsh/npc_team/")
 
 setup(
     name="npcsh",
-    version="0.3.23",
+    version="0.3.24",
     packages=find_packages(exclude=["tests*"]),
     install_requires=[
         "redis",
