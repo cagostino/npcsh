8544a714d4b1f6906dd6e4b419e1e187c334a577
Chris Agostino <cjp.agostino@gmail.com>
2025-03-22 18:46:16 -0400

    pyver

--- setup.py
+++ setup.py
@@ -99,7 +99,7 @@ extra_files = package_files("npcsh/npc_team/")
 
 setup(
     name="npcsh",
-    version="0.3.27.1",
+    version="0.3.27.2",
     packages=find_packages(exclude=["tests*"]),
     install_requires=base_requirements,  # Only install base requirements by default
     extras_require={
