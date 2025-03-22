25a1de9d467e33e37d7599ffa60935f584f91982
Chris Agostino <cjp.agostino@gmail.com>
2025-03-22 19:08:12 -0400

    whisper pyver

--- setup.py
+++ setup.py
@@ -102,7 +102,7 @@ extra_files = package_files("npcsh/npc_team/")
 
 setup(
     name="npcsh",
-    version="0.3.27.5",
+    version="0.3.27.6",
     packages=find_packages(exclude=["tests*"]),
     install_requires=base_requirements,  # Only install base requirements by default
     extras_require={
