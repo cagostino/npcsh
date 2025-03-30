7faf47716120e5fe370d4132809f033cfc66f08d
Chris Agostino <cjp.agostino@gmail.com>
2025-03-30 19:17:47 -0400

    another

--- docs/conf.py
+++ docs/conf.py
@@ -1,69 +1,44 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+import os
+import sys
+from pathlib import Path
 
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
-
-
-# -- Project information -----------------------------------------------------
+# Add the project root to sys.path
+sys.path.insert(0, os.path.abspath(".."))
 
 project = "npcsh"
 copyright = "2025, Christopher Agostino"
 author = "Christopher Agostino"
 
+# Extensions
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.viewcode",
+]
 
-# -- General configuration ---------------------------------------------------
+# Show full module paths
+add_module_names = True
 
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
-
-
-# Add any paths that contain templates here, relative to this directory.
-templates_path = ["_templates"]
-
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
-
-
-# -- Options for HTML output -------------------------------------------------
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-html_theme = "alabaster"
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
-
-import os
-import sys
-
-sys.path.insert(0, os.path.abspath(".."))
-
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon", "sphinx.ext.autosummary"]
-
-# Make autosummary find and document everything
+# Autosummary settings
 autosummary_generate = True
-add_module_names = False  # Makes names cleaner
+
+# Autodoc settings
 autodoc_default_options = {
     "members": True,
-    "member-order": "bysource",
-    "special-members": "__init__",
     "undoc-members": True,
-    "exclude-members": "__weakref__",
+    "show-inheritance": True,
+    "imported-members": True,
 }
+
+# General settings
+templates_path = ["_templates"]
+exclude_patterns = ["_build"]
+html_theme = "alabaster"
+html_static_path = ["_static"]
+
+# This is important - it ensures all modules are importable
+import npcsh
+import pkgutil
+
+all_modules = [name for _, name, _ in pkgutil.iter_modules(npcsh.__path__, "npcsh.")]
